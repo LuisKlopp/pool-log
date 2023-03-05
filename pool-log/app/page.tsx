b0f724d6f65aed50915f5070f2367544c50bd4d6
LuisKlopp <fbgus333@gmail.com>
2023-03-05 15:59:59 +0900

    feat: 정적파일생성

--- pool-log/app/page.tsx
+++ pool-log/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
-  return (
-    <>
-      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">dd</div>
-    </>
-  );
-}
+import Link from "next/link";
+
+import getPostMetadata from "@/components/getPostMetadata";
+
+const Home = () => {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((post) => (
+    <div key={post.slug}>
+      <Link href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
+      </Link>
+      <p>{post.subtitle}</p>
+      <p>{post.date}</p>
+    </div>
+  ));
+  return <div>{postPreviews}</div>;
+};
+export default Home;
--- pool-log/tailwind.config.js
+++ pool-log/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
